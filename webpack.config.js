e993ca6c88af6bbe4ea775089456d39a2e0d2636
Radoslav Tomas <radoslav.tomas@gmail.com>
2019-10-22 14:27:02 +0100

    implemented postcss loader + clean before merge

--- webpack.config.js
+++ webpack.config.js
@@ -4,53 +4,48 @@ const VueLoaderPlugin = require("vue-loader/lib/plugin");
 const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "index.js",
-        libraryTarget: "umd",
-        library: "SimpleKit"
-    },
-    mode: "development",
-    target: "web",
-    externals: [
-        nodeExternals({
-            importType: "umd"
-        })
-    ],
-    resolve: {
-        alias: {
-            node_modules: path.join(__dirname, "node_modules")
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    libraryTarget: "umd",
+    library: "SimpleKit"
+  },
+  mode: "development",
+  target: "web",
+  externals: [
+    nodeExternals({
+      importType: "umd"
+    })
+  ],
+  resolve: {
+    alias: {
+      node_modules: path.join(__dirname, "node_modules")
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
         }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env"]
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                exclude: /(node_modules|bower_components)/,
-                use: [
-                    "style-loader",
-                    {
-                        loader: "css-loader"
-                    }
-                ]
-            },
-            {
-                test: /\.vue$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: "vue-loader"
-            }
-        ]
-    },
-    plugins: [new VueLoaderPlugin()]
+      },
+      {
+        test: /\.css$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ["style-loader", "css-loader", "postcss-loader"]
+      },
+      {
+        test: /\.vue$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "vue-loader"
+      }
+    ]
+  },
+  plugins: [new VueLoaderPlugin()]
 };
